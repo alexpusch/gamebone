9466658b14e6cbec1988f78855e292cb01a497bd
Alex Puschinsky <alexpoo@gmail.com>
2015-07-05 00:14:47 +0300

    Created TestGraphicsAdapter
    Made sure that tests will not load pixi graphics adapter, but a fake and
    simple graphcis adapter

---
+++ test/helpers/test_graphics_adapter.js
@@ -0,0 +1,16 @@
+import * as sinon from "imports?define=>false&require=>false!sinon/pkg/sinon"
+
+let TestGraphicsAdapeter = {
+  createContainer(){
+    return {
+      addChild: sinon.spy(),
+      removeChild: sinon.spy()
+    }
+  },
+
+  createStage: sinon.spy(),
+
+  render: sinon.spy()
+}
+
+export default TestGraphicsAdapeter;
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,7 @@
+import { setGraphicsAdapter } from "../src/graphics_adapters"
+import TestGraphicsAdapeter from "./helpers/test_graphics_adapter"
+setGraphicsAdapter(TestGraphicsAdapeter);
+
 import {} from "./events_test"
 import {} from "./game_test"
 import {} from "./view_test"
