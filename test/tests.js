5789c1544ad4a1762828bdbfaae32dc42b8bf37f
Alex Puschinsky <alexpoo@gmail.com>
2015-12-30 17:58:47 +0200

    Require all tests at once with require.context

--- test/tests.js
+++ test/tests.js
@@ -6,15 +6,8 @@ before(function(done){
   setTimeout(done, 200);
 });
 
-import {} from "./events_test"
-import {} from "./game_test"
-import {} from "./view_test"
-import {} from "./collection_test"
-import {} from "./model_test"
-import {} from "./collection_view_test"
-import {} from "./controls_test"
-import {} from "./observe_test"
-import {} from "./request_response_test"
-import {} from "./mixin_test"
-import {} from "./phyisics_mixin_test"
-import {} from "./world_test"
+let testContext = require.context(".", false, /.+_test\.js/);
+
+testContext.keys().forEach(function(key){
+  testContext(key);
+})
